e193b29d03f28210a57c960f0a0f11272b0c5f32
pulsar <pulsar@cca996d7-57bb-40df-b7ff-243d9995c488>
2009-03-06 12:24:23 +0000

    Added features related to new subbanding in ASPFitsReader:
    -nsubs: Number of contiguous output subbands in which to divide input band
    -nchansub: Number of channels to include in each (contiguous) output subband
    -subbw: Width of each (contiguous) output subband in MHz
    -subref: Chosen reference frequency for creating subbands
    
    This will soon (but not quite yet) replace the -addchans option.  
    
    For example, -nsubs 1 is the same as -addchans.
    
    In the future, will  need to add option to choose output subband frequencies explicitly, 
    e.g., -subfreq 1372 1392 1396 1396 1400 1402
    
    and so on, so that subband widths need not be equal.  Once this is done, then we can fully make -addchans obsolete.
    
    See (upcoming) documentation for usage of these options 
    
    
    
    git-svn-id: svn+ssh://nanograv.astro.ubc.ca/data1/svn/repos/ASPFitsReader@12 cca996d7-57bb-40df-b7ff-243d9995c488

--- src/GetChans.c
+++ src/GetChans.c
@@ -9,16 +9,32 @@
 int GetChans(struct ASPHdr *hdr, Cmdline *Cmd, struct RunVars *RunMode)
 {
 
-  int  i, j, i_chan, ChanIndex;
-  int  notpicked; 
-  int  test_chan_file=0;
+  int    i, j, i_chan, i_chan_in, i_chan_out, ChanIndex;
+  int    chan_up=0, chan_down=0, chan_hi=0, chan_lo=0, chan_start=-1;
+  int    notpicked; 
+  int    test_chan_file=0;
+  int    NChanSub;
+  double BW;
   FILE *Ftest;
+
+  int testarray[10];
+
+  BW = DSum(hdr->obs.ChanWidth, hdr->obs.NChan);
 
   /* START OF CHANNEL ADDING OPTION PARSING */
   /* Check to see if there are chans to add: */
   RunMode->AddChans = Cmd->AddChansP;
-   
+  
+
+
   if(Cmd->AddChansP) {
+
+    if(Cmd->NSubsP || Cmd->SubBW || Cmd->SubRefP) {
+      printf("Cannot use -nsubs, -subbw, or -subref when using -addchans. ");
+      printf("Exiting...\n");
+      return -19;
+    }
+
     if(Cmd->AddChansC%2 > 0) {  /* if not even number of args */
       printf("-addchans:  must have even number of arguments (currently you have %d)! Exiting...\n",Cmd->AddChansC);
       fflush(stdout);
@@ -169,6 +185,228 @@ int GetChans(struct ASPHdr *hdr, Cmdline *Cmd, struct RunVars *RunMode)
       RunMode->NOutChans++;
       
     }
+  }
+  /* Subband options */
+  else if(Cmd->NSubsP || Cmd->SubBWP || Cmd->NChanSubP || Cmd->SubRefP) {
+    /* Make sure that the user did not combine subband channel-finding
+       options */
+    if ( (Cmd->NSubsP && Cmd->SubBWP) ||
+	 (Cmd->NSubsP && Cmd->NChanSubP) ||
+	 (Cmd->SubBWP && Cmd->NChanSubP) ){
+      printf("Can only use one of -nchansub, -subbw, or -nsubs at a time. ");
+      printf("Exiting...\n");
+      return -19;
+    }
+    /* Make sure that NSubs is > 0 and < NChans */
+    if(Cmd->NSubsP && (Cmd->NSubs < 1) ) {
+      printf("Argument to -nsubs must be greater than zero.  Exiting...\n");
+      return -19;
+    }
+    if(Cmd->NSubsP && (Cmd->NSubs > hdr->obs.NChan) ) {
+      printf("Argument to -nsubs cannot be more than number of channels in \n");
+      printf("data file (%d).  Exiting...\n",hdr->obs.NChan);
+      return -19;
+    }
+    /* Make sure that NChanSub is > 0 and < NChans */
+    if(Cmd->NChanSubP && (Cmd->NChanSub < 1) ) {
+      printf("Argument to -nchansub must be greater than zero.  Exiting...\n");
+      return -19;
+    }
+    if(Cmd->NChanSubP && (Cmd->NChanSub > hdr->obs.NChan) ) {
+      printf("Argument to -nchansub cannot be more than number of channels \n");
+      printf("in data file (%d).  Exiting...\n",hdr->obs.NChan);
+      return -19;
+    }
+    /* Make sure that if -subref is used, then one of -nsubs -subbw, 
+       or -nchansub is also used */
+    if(Cmd->SubRefP && !(Cmd->NSubsP || Cmd->SubBWP || Cmd->NChanSubP)){
+      printf("Must use one of the -nsubs, -subbw, or -nchansub options when ");
+      printf("using the -subref option to fix a reference subband ");
+      printf("frequency. Exiting...\n");
+      fflush(stdout);
+      return -19;
+    }
+
+    /* Now calculate the individual subband bandwidth, 
+       in number of channels (i.e. not in MHz).  This is done by rounding off 
+       (total channels)/(nsubs) to nearest integer number of channels */
+    printf("%d input channels\n", hdr->obs.NChan);
+    if(Cmd->NSubsP) {
+      NChanSub = (int)(floor((double)hdr->obs.NChan/(double)Cmd->NSubs));
+      RunMode->NOutChans = Cmd->NSubs;
+      printf("%d subbands chosen.\n", Cmd->NSubs);
+      printf("%.1lf MHz subband width\n", NChanSub*(BW/(double)hdr->obs.NChan));
+    }
+    else if(Cmd->SubBWP){ 
+      /* Here, round off to nearest multiple of actual input channel BW */
+      NChanSub = (int)(floor(Cmd->SubBW/(BW/(double)hdr->obs.NChan) +0.5));
+      RunMode->NOutChans = (int)(ceil((double)hdr->obs.NChan/(double)NChanSub));
+      printf("%.1lf-MHz subbands chosen.\n", Cmd->SubBW);
+    }
+    else { // Cmd->NChanSub:
+      NChanSub = Cmd->NChanSub;
+      RunMode->NOutChans = (int)(ceil((double)hdr->obs.NChan/(double)NChanSub));
+      printf("%.1lf MHz subband width\n", NChanSub*(BW/(double)hdr->obs.NChan));
+    }
+    printf("%d channels per subband.\n",NChanSub);
+
+
+    /* If user has chosen a reference frequency for subbanding: */
+    if(Cmd->SubRefP) {
+      /* Make sure that -subref argument corresponds to the boundary of 
+	 two channels */
+      chan_hi=0;
+      chan_lo=0;
+      for(i_chan_in=0; i_chan_in<hdr->obs.NChan; i_chan_in++){
+	if (Cmd->SubRef == hdr->obs.ChanFreq[i_chan_in] - 2.)
+	  chan_hi++;
+	if (Cmd->SubRef == hdr->obs.ChanFreq[i_chan_in] + 2.)
+	  chan_lo++;
+      }
+
+      chan_up = chan_down = 0;
+      if(chan_hi + chan_lo == 0) { // bad choice of ref frequency
+	printf("Argument to -subref MUST be a frequency (in MHz) that ");
+	printf("borders two existing channel frequencies.  The following are ");
+	printf("the centre frequencies of the channels in the current data ");
+	printf("file:\n\n");
+	for(i_chan_in=0; i_chan_in<hdr->obs.NChan; i_chan_in++){
+	  printf("%.1lf   ",hdr->obs.ChanFreq[i_chan_in]);
+	}
+	printf("\n\nAnd thus these are the choices of -subref arguments:\n\n");
+	printf("%.1lf   ",hdr->obs.ChanFreq[0]-
+	       RunMode->Sideband*2.);
+	for(i_chan_in=0; i_chan_in<hdr->obs.NChan; i_chan_in++){
+	  printf("%.1lf   ",hdr->obs.ChanFreq[i_chan_in]+
+		 RunMode->Sideband*2.);
+	}
+	/*	printf("%.1lf   ",hdr->obs.ChanFreq[hdr->obs.NChan-1]+
+		RunMode->Sideband*2.); */
+	printf("\n\n");
+	return -19;
+      }
+      /* Get start channel */
+      else if(chan_hi + chan_lo == 1){  /* We are at one of the end frequecies */
+	chan_start = Freq2Chan(Cmd->SubRef + 2., 
+			      hdr->obs.ChanFreq, hdr->obs.NChan);
+	if (chan_start < 0) { /* oops, went wrong direction */
+	  chan_start = Freq2Chan(Cmd->SubRef - 2., 
+			      hdr->obs.ChanFreq, hdr->obs.NChan);
+	}
+	/* Now find out if user chose 0th or NChan-th channel */
+	if (chan_start == 0) chan_up=1;
+	else /* (chan_start == hdr->obs.NChan-1) */ chan_down=1;
+      }
+      else{ /* channel lands somewhere in the middle */
+	/* Choose channel with  next lowest centre frequency to start */
+	chan_start = Freq2Chan(Cmd->SubRef - 2., 
+			       hdr->obs.ChanFreq, hdr->obs.NChan);
+      }
+
+  /*  printf("chan_up = %d, chan_down = %d, chan_hi = %d, chan_lo = %d\n",
+	     chan_up, chan_down, chan_hi, chan_lo);
+      printf("SubRef = %.1lf,  chan_start = %d\n", Cmd->SubRef, chan_start); */
+
+      /* Now go through two loops:  One going down in channel index and 
+	 one going up in channel number.  Start at chan_start. */
+      
+      /* Reset NOutChans and count as we go along, just in case there is some
+	 issue with how the subbands are distributed */
+      RunMode->NOutChans=0;
+      /* Going down...*/
+      for (i_chan_in=chan_start; i_chan_in>=0; i_chan_in-=NChanSub){
+      /* Ensure that first output channel begins at input channel zero */
+	if(i_chan_in-NChanSub+1 < 0)
+	  RunMode->FirstChanAdd[RunMode->NOutChans]=0;
+	else
+	  RunMode->FirstChanAdd[RunMode->NOutChans]=i_chan_in-NChanSub+1;
+	RunMode->LastChanAdd[RunMode->NOutChans]=i_chan_in;	  
+ 	RunMode->NOutChans++;
+      }
+      /* Going up... start at the next channel index up from chan_start 
+	 since we started from chan_start previously */
+      for (i_chan_in=chan_start+1;i_chan_in<hdr->obs.NChan;i_chan_in+=NChanSub){
+	RunMode->FirstChanAdd[RunMode->NOutChans]=i_chan_in;
+	/* Ensure that last output channel end at final input channel */
+	if(i_chan_in+NChanSub-1 > hdr->obs.NChan-1)
+	  RunMode->LastChanAdd[RunMode->NOutChans]=hdr->obs.NChan-1;
+	else
+	  RunMode->LastChanAdd[RunMode->NOutChans]=i_chan_in+NChanSub-1;
+	RunMode->NOutChans++;
+      }
+
+      /* Now sort First/LastChanAdd arrays.  A straight sort of each should 
+	 do since there should not be any overlap between a LastChanAdd and a
+	 FirstChanAdd of another set */
+
+      if (Cmd->VerboseP){
+	printf("SUBREF PRE-SORT:\n");
+	for (i_chan_out=0; i_chan_out<RunMode->NOutChans; i_chan_out++)
+	  printf("%d   ",RunMode->FirstChanAdd[i_chan_out]);
+	printf("\n");
+	for (i_chan_out=0; i_chan_out<RunMode->NOutChans; i_chan_out++)
+	  printf("%.0lf ",hdr->obs.ChanFreq[RunMode->FirstChanAdd[i_chan_out]]);
+	printf("\n\n");
+	for (i_chan_out=0; i_chan_out<RunMode->NOutChans; i_chan_out++)
+	  printf("%d   ",RunMode->LastChanAdd[i_chan_out]);
+	printf("\n");
+	for (i_chan_out=0; i_chan_out<RunMode->NOutChans; i_chan_out++)
+	  printf("%.0lf ",hdr->obs.ChanFreq[RunMode->LastChanAdd[i_chan_out]]);
+	printf("\n\n");
+      }
+      
+      ISort(RunMode->NOutChans, &RunMode->FirstChanAdd[0]);
+      ISort(RunMode->NOutChans, &RunMode->LastChanAdd[0]);
+      
+      
+      if (Cmd->VerboseP) {
+	printf("SUBREF POST-SORT:\n");
+	for (i_chan_out=0; i_chan_out<RunMode->NOutChans; i_chan_out++)
+	  printf("%d   ",RunMode->FirstChanAdd[i_chan_out]);
+	printf("\n");
+	for (i_chan_out=0; i_chan_out<RunMode->NOutChans; i_chan_out++)
+	  printf("%.0lf ",hdr->obs.ChanFreq[RunMode->FirstChanAdd[i_chan_out]]);
+	printf("\n\n");
+	for (i_chan_out=0; i_chan_out<RunMode->NOutChans; i_chan_out++)
+	  printf("%d   ",RunMode->LastChanAdd[i_chan_out]);
+	printf("\n");
+	for (i_chan_out=0; i_chan_out<RunMode->NOutChans; i_chan_out++)
+	  printf("%.0lf ",hdr->obs.ChanFreq[RunMode->LastChanAdd[i_chan_out]]);
+	printf("\n\n");
+      }
+      
+      //      exit(0);
+    }
+    else {
+      /* If no reference frequency chosen, start subbanding from channel zero */
+      /* Now fill in FirstChanAdd and LastChanAdd, so that new subbanded data 
+	 channels are contiguous */
+      for (i_chan_out=0; i_chan_out<RunMode->NOutChans; i_chan_out++){
+	RunMode->FirstChanAdd[i_chan_out] = i_chan_out*NChanSub;
+	if( ((i_chan_out+1)*NChanSub - 1) > hdr->obs.NChan-1)
+	  RunMode->LastChanAdd[i_chan_out] = hdr->obs.NChan-1;
+	else
+	  RunMode->LastChanAdd[i_chan_out] = (i_chan_out+1)*NChanSub - 1;
+      }
+      /* Finally, check that last output channel extends to include last 
+	 input channel*/
+      RunMode->LastChanAdd[RunMode->NOutChans-1] = hdr->obs.NChan-1;
+    }
+    
+
+    printf("%d output subbbands over %.1lf MHz bandwidth:\n\n", 
+	   RunMode->NOutChans, BW);
+    printf("START   ");
+    for (i_chan_out=0; i_chan_out<RunMode->NOutChans; i_chan_out++)
+      printf("%6.1lf   ", hdr->obs.ChanFreq[RunMode->FirstChanAdd[i_chan_out]]);
+    printf("\nEND     ");
+    for (i_chan_out=0; i_chan_out<RunMode->NOutChans; i_chan_out++)
+      printf("%6.1lf   ", hdr->obs.ChanFreq[RunMode->LastChanAdd[i_chan_out]]);
+    printf("\n\n");
+
+    //  exit(0);
+   
+
   }
   else{
     /* Channels transfer exactly if we are not adding channels */
