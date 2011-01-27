b52ccc7ecd2ddb9cd5599a6da2eb9d2a69fa9997
pulsar <pulsar@cca996d7-57bb-40df-b7ff-243d9995c488>
2011-01-27 23:52:35 +0000

    Little include file, mostly to assign various pgplot number definitions into words, e.g. black=1, etc.
    -- R. Ferdman, 27 January 2011
    
    
    
    
    git-svn-id: svn+ssh://nanograv.astro.ubc.ca/data1/svn/repos/ASPFitsReader@73 cca996d7-57bb-40df-b7ff-243d9995c488

---
+++ include/cpgplot_def.h
@@ -0,0 +1,24 @@
+/* Colours */
+
+#define c_black 0
+#define c_white 1
+#define c_red 2
+#define c_green 3
+#define c_blue 4
+#define c_cyan 5
+#define c_magenta 6
+#define c_yellow 7
+
+/* Line style */
+
+#define l_full 1
+#define l_dashed 2
+#define l_dotdash 3
+#define l_dotted 4
+#define l_fancy 5  // = dash-dot-dot-dot
+
+/* Character font */
+#define ch_normal 1
+#define ch_roman 2
+#define ch_italic 3
+#define ch_script 4
